9d76137b37a9195de0a7ae26246afd45d6ae39c7
Shanu Sharma <shanu5544@gmail.com>
2024-05-12 22:49:19 +0530

    feat(app): add basic http server using actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web :: {get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn health(req:HttpRequest)->impl Responder{
+    println!("api hit {:?}",req);
+    HttpResponse::Ok().body("Healthy")
+}
+
+#[get("/hello")]
+async fn hello()->impl Responder{
+    HttpResponse::Ok().body("Hello There")
+}
+
+#[actix_web::main]
+async fn main()-> std::io::Result<()>{
+    HttpServer::new(||{
+        App::new()
+        .service(hello)
+        .route("/health", web::get().to(health))
+    }).bind(("127.0.0.1", 3000))?
+    .run()
+    .await
 }
